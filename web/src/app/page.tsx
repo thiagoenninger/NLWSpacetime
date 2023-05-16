7bbec4f4d4489a25b820bc07d1d66bd0f457ed48
Thiago Enninger <thiagoenninger@gmail.com>
2023-05-16 19:54:16 -0300

    finalização do setup web com next.js

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="m-10 h-20 w-20 rounded bg-blue-400 text-2xl font-bold">
-      oi
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )
 }
